422de006e0e421d3a020333c463f583f7ae8a436
PrimordialOrigin <RamerafheKaone@protonmail.com>
2025-06-15 14:56:43 +0200

    initial commit

---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express, { NextFunction, Request, Response } from "express";
+import { logger } from "./logger";
+import bodyParser from "body-parser";
+import cors from "cors"
+
+const app = express();
+
+
+
+export default app;
